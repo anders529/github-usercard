a4a5db2dfd524b63ed19632462ea63d0f4ee833c
Aaron Anderson <anderseana@tutanota.com>
2019-12-06 00:26:32 -0600

    took all night, but i got it done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,146 +1,96 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
-//Gonna pull some info from GitHub./
-
-axios.get('https://api.github.com/users/anders529')
-  .then(data => {
-    console.log('Success! ', data);
-    const cards = document.querySelector('.cards');
-    cards.appendChild(createCard(data.data));
-  })
-    .catch(err => {
-      console.log('Error: ', err)
-    })
-
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-    // Going to pull follower info from GitHub
-    axios.get('https://api.github.com/users/anders529/followers')
-        .then(data => {
-          console.log('Dang, that actually worked! Here is a list of followers:' ,data.data);
-          const followersData = data.data;
-          followersData.forEach(followerData => {
-              followersArray.push(followerData.login);            
-          });
-        })
-    followersArray.forEach(follower => {
-      axios.get('https://api.github.com/users/${follower}')
-        .then(data => {
-          console.log('Follower Info: ', data.data);
-          const cards2 = document.querySelector('.cards');
-          cards2.appendChild(createCard(data.data));
-        })
-
-        .catch(err => {
-          console.log("Couldn't get follower info:", err);
-        })
-    })
-
-    .catch(err => {
-      console.log('There was an issue trying to get followers. Sorry!', err)
-    })
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
+// // THIS REALLY SHOULD HAVE BEEN STEP ONE!!! THIS TOOK ME FOREVER TO FIGURE OUT!!! >:C
 const followersArray = [
-    'tetondan',
-    'dustinmyers',
-    'justsml',
-    'luishrd',
-    'bigknell'
-  ];
+'tetondan',
+'dustinmyers',
+'justsml',
+'luishrd',
+'bigknell'
+];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+// Gonna pull some info from GitHub.
+axios.get('https://api.github.com/users/anders529')
+.then(data => {
+console.log('Successful!', data);
+const cards = document.querySelector('.cards');
+cards.appendChild(makeCards(data.data));
+})
+.catch(err => {
+console.log('Error:', err);
+})
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+// Going to grab Follower Information as well.
 
-*/
+axios.get('https://api.github.com/users/anders529/followers')
+.then(data =>{
+console.log('Wow! That actually worked! Here is the list of your dumb followers:', data.data);
+const followersData = data.data;
+followersData.forEach(followerData =>{
+  followersArray.push(followerData.login);
+})
 
-// Adding DOM elements and stuff.
+followersArray.forEach(follower => {
+axios.get(`https://api.github.com/users/${follower}`)
+.then(data => {
+  console.log('Follower info: ' , data.data );
+  const cards2 = document.querySelector('.cards');
+  cards2.appendChild(makeCards(data.data));
+})
 
-const usrCrd = document.createElement('div');
-const usrImg = document.createElement('img');
-const UsrInfo = document.createElement('div');
-const usrNme = document.createElement('h3');
-const displName = document.createElement('p');
-const usrLocation = document.createElement('p');
-const profl = document.createElement('p');
-const usrProfl = document.createElement('a');
-const usrflwrs = document.createElement('p');
-const usrFlwng = document.createElement('p');
-const usrBio = document.createElement('p');
+.catch(err => {
+  console.log('I cannot get any follower information: ', err);
+})
+})
+})
 
-// Going to add some classes.
+.catch(err => {
+console.log ('Houston, we have a problem: ' , err);
+})
 
-  usrCrd.classList.add('card');
-  usrInfo.classList.add('card-info');
-  usrNme.classList.add('name');
-  displyNme.classList.add('username');
+function makeCards(data){
 
-  // I hate assigning values to shit.
+// // Adding DOM elements and stuff.
+  const usrCrd = document.createElement('div');
+  const img = document.createElement('img');
+  const info = document.createElement('div');
+  const usrName = document.createElement('h3');
+  const displayName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const usrProfile = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
 
-  usrImg.src = data.avatar_url;
-  usrNme.textContent = data.name;
-  displyNme.textContent = data.login;
-  usrLocation.textContent = 'Location: ${data.location}';
-  usrProfl.innerHTML = 'Profile: <a href=${data.html_url} > ${data.html_url}</a>';
-  usrflwrs.textContent = 'Followers: ${data.followers}';
-  usrFlwng.textContent = 'Following: ${data.following}';
-  usrBio.textContent = 'Bio: ${data.bio}';
+// // Going to add some classes.
 
-  // WHY DO I HAVE TO STRUCTURE THIS CRAP!? >:C GO AWAY!!! APPENDING CRAP SUCKS!!!
+    usrCrd.classList.add('card');
+    info.classList.add('card-info');
+    usrName.classList.add('name');
+    displayName.classList.add('username');
 
-  usrCrd.appendChild(usrImg);
-  usrCrd.appendChild(usrInfo);
-  usrInfo.appendChild(usrNme);
-  usrInfo.appendChild(displNme);
-  usrInfo.appendChild(usrLocation);
-  usrInfo.appendChild(profl);
-  profl.appendChild(usrProfl);
-  usrInfo.appendChild(usrflwrs);
-  usrInfo.appendChild(usrFlwng);
-  usrInfo.appendChild(usrBio);
+//   // WHY DO I HAVE TO STRUCTURE THIS CRAP!? >:C GO AWAY!!! APPENDING CRAP SUCKS!!!
 
-  return usrCrd;
+    usrCrd.appendChild(img);
+    usrCrd.appendChild(info);
+    info.appendChild(usrName);
+    info.appendChild(displayName);
+    info.appendChild(location);
+    info.appendChild(profile);
+    profile.appendChild(usrProfile);
+    info.appendChild(followers);
+    info.appendChild(following);
+    info.appendChild(bio);
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+//  I hate assigning values to shit.
+
+    img.src = data.avatar_url;
+    usrName.textContent = data.name;
+    displayName.textContent = data.login;
+    location.textContent = `Location: ${data.location}`;
+    usrProfile.textContent = `Profile: <a href=${data.html_url}>${data.html_url}</a>`;
+    followers.textContent = `Followers: ${data.followers}`;
+    following.textContent = `Following: ${data.following}`;
+    bio.textContent = `Bio: ${data.bio}`;
+
+    return usrCrd;
+  }
